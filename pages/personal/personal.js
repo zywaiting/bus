417d9935b9576c3f86802ad5abf5f49b1ef66320
wq_zy <1573240324@qq.com>
2018-10-16 21:18:22 +0800

    ...

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -5,7 +5,122 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    hotList: [
+      {
+        pic: "http://www.lemontreevip.com/upload/201512/201512191450513761.jpg",
+        title: "春风十里不如你",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "南若北",
+        fee: "3000",
+        experience: '7年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201512/201512191450512739.jpg",
+        title: "一场没有终点的旅行",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "苏晓",
+        fee: "4000",
+        experience: '10年以上'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201603/1458298749772440744000.jpg",
+        title: "恋上彩虹的梦",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "峰方",
+        fee: "3500",
+        experience: '10年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201512/201512101449733988.jpg",
+        title: "日暮三亚",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "大鹏",
+        fee: "2500",
+        experience: '7年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201508/1438966134523121267000.jpg",
+        title: "帆船高尔夫",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "一方",
+        fee: "2500",
+        experience: '7年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201603/201603181458295704.jpg",
+        title: "西岛恋曲",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "大浦",
+        fee: "1800",
+        experience: '5年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201512/201512191450512739.jpg",
+        title: "一场没有终点的旅行",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "苏晓",
+        fee: "4000",
+        experience: '10年以上'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201603/1458298749772440744000.jpg",
+        title: "恋上彩虹的梦",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "峰方",
+        fee: "3500",
+        experience: '10年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201512/201512101449733988.jpg",
+        title: "日暮三亚",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "大鹏",
+        fee: "2500",
+        experience: '7年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201508/1438966134523121267000.jpg",
+        title: "帆船高尔夫",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "一方",
+        fee: "2500",
+        experience: '7年'
+      },
+      {
+        pic: "http://www.lemontreevip.com/upload/201603/201603181458295704.jpg",
+        title: "西岛恋曲",
+        area: "三亚",
+        day: "1day",
+        avatar: "http://bao.lanrenmb.com/Public/dist/img/user2-160x160.jpg",
+        name: "大浦",
+        fee: "1800",
+        experience: '5年'
+      }
+    ]
   },
 
   /**
